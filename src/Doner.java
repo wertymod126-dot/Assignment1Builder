8cafa3837e4e65d9da9d1b31d922a45c5625af20
werty <wertymod126@gmail.com>
2026-09-11 20:03:10 +0500

    major changes

--- src/Doner.java
+++ src/Doner.java
@@ -1,16 +1,7 @@
-enum WrapType {
-    Lavash,
-    CheeseLavash,
-    FriedLavash,
-}
-
-enum MeatType {
-    Beef,
-    Chicken,
-    MixedMeat,
-}
-
 public class Doner {
+
+    public enum WrapType { Lavash, CheeseLavash, FriedLavash }
+    public enum MeatType { Beef, Chicken, MixedMeat }
 
     private final WrapType wrap;
     private final MeatType meat;
@@ -21,9 +12,8 @@ public class Doner {
     private final boolean hasGarlicSauce;
     private final boolean isSpicy;
 
-    //constructor
 
-    protected Doner(DonerBuilder builder) {
+    private Doner(Builder builder) {
         this.wrap = builder.wrap;
         this.meat = builder.meat;
         this.hasTomato = builder.hasTomato;
@@ -38,5 +28,40 @@ public class Doner {
     public String toString() {
         return String.format("Doner----------------- \n-----Wrap = %s,\n -----Meat = %s,\n -----Tomato = %b,\n -----Pickles = %b,\n -----Onion = %b,\n -----FriesInside = %b,\n -----GarlicSauce = %b,\n -----Spicy = %b\n",
                 wrap, meat, hasTomato, hasPickles, hasOnion, hasFries, hasGarlicSauce, isSpicy);
+    }
+
+
+    public static class Builder {
+
+
+        private WrapType wrap;
+        private MeatType meat;
+        private boolean hasTomato = true;
+        private boolean hasPickles = true;
+        private boolean hasOnion = false;
+        private boolean hasFries = true;
+        private boolean hasGarlicSauce = false;
+        private boolean isSpicy = false;
+
+        public Builder withWrap(WrapType wrap) { this.wrap = wrap; return this; }
+        public Builder withMeat(MeatType meat) { this.meat = meat; return this; }
+        public Builder withFries(boolean hasFries) { this.hasFries = hasFries; return this; }
+        public Builder withGarlicSauce(boolean hasGarlicSauce) { this.hasGarlicSauce = hasGarlicSauce; return this; }
+        public Builder withPickles(boolean hasPickles) { this.hasPickles = hasPickles; return this; }
+        public Builder withOnion(boolean isOnion) { this.hasOnion = isOnion; return this; }
+        public Builder withTomato(boolean hasTomato) { this.hasTomato = hasTomato; return this; }
+        public Builder makeSpicy(boolean isSpicy) { this.isSpicy = isSpicy; return this; }
+
+
+
+        public Doner build() {
+            validateOrder();
+            return new Doner(this);
+        }
+
+        private void validateOrder() {
+            if (wrap == null) throw new IllegalStateException("Missing wrap.");
+            if (meat == null) throw new IllegalStateException("Missing meat.");
+        }
     }
 }
